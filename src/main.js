3fee1720ee50df13679c98a7642e0dbc8286fd8b
Alex Siegberg <alex.siegberg@gmail.com>
2019-07-05 11:31:09 -0700

    properly outout doctor cards to page

--- src/main.js
+++ src/main.js
@@ -5,43 +5,43 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 let generateDoctorCard = (doctorFirstName, doctorLastName, image, acceptingNewPatience, address) => {
-  // let htmlForDoctor = `<div class='col-md-4'>
-  //   <div class="card" style="width: 18rem;">
-  //   <img class="card-img-top" src="${image}" alt="Card image cap">
-  //   <div class="card-body">
-  //   <h5 class="card-title">${doctorLastName} ${doctorLastName}</h5>
-  //   </div>
-  //   <ul class="list-group list-group-flush">
-  //     <li class="list-group-item">Address: ${address}</li>
-  //     <li class="list-group-item">Accepting new patients: ${acceptingNewPatience}</li>
-  //   </ul>
-  //   </div>
-  // </div>`;
+  let htmlForDoctor =
+  `<div class='col-md-3'>
+    <div class="card">
+      <img class="card-img-top" src="${image}" alt="Card image cap">
+      <div class="card-body">
+      <h5 class="card-title">${doctorFirstName} ${doctorLastName}</h5>
+      </div>
+      <ul class="list-group list-group-flush">
+        <li class="list-group-item">Address: ${address}</li>
+        <li class="list-group-item">Accepting new patients: ${acceptingNewPatience}</li>
+      </ul>
+    </div>
+  </div>`;
 
-  console.log(`this is a last name: ${doctorLastName}`);
-  $('#doctors').text = doctorLastName;
+  $('#doctors').append(htmlForDoctor);
 };
 
 $(document).ready(function () {
   $('#doctorForm').submit(function (event) {
     event.preventDefault();
+    $('#doctors').html('');
     const query = $('#medIssue').val();
     let service = new DoctorList;
     let promise = service.getDoctor(query);
 
     promise.then(function (response) {
       let body = JSON.parse(response);
-      console.log(body);
       body.data.forEach(function (doctor) {
+
         const firstName = doctor.profile.first_name;
         const lastName = doctor.profile.last_name;
         const image = doctor.profile.image_url;
-        console.log(`${firstName} ${lastName}\n${image}`);
+
         doctor.practices.forEach(function (practice) {
           if (practice.within_search_area) {
             const acceptingNewPatience = practice.accepts_new_patients;
-            const address = `${practice.visit_address.city} ${practice.visit_address.state}, ${practice.visit_address.zip}
-            \n${practice.visit_address.street}`;
+            const address = `${practice.visit_address.city} ${practice.visit_address.state}, ${practice.visit_address.zip}\n${practice.visit_address.street}`;
             generateDoctorCard(firstName, lastName, image, acceptingNewPatience, address);
           }
         });
