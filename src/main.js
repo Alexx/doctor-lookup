89d74b075dbc5bf7f4c7ad94f74f4b5a88914af6
Alex Siegberg <alex.siegberg@gmail.com>
2019-07-05 11:10:47 -0700

    attempt to output doctor info, in progress

--- src/main.js
+++ src/main.js
@@ -4,6 +4,24 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+let generateDoctorCard = (doctorFirstName, doctorLastName, image, acceptingNewPatience, address) => {
+  // let htmlForDoctor = `<div class='col-md-4'>
+  //   <div class="card" style="width: 18rem;">
+  //   <img class="card-img-top" src="${image}" alt="Card image cap">
+  //   <div class="card-body">
+  //   <h5 class="card-title">${doctorLastName} ${doctorLastName}</h5>
+  //   </div>
+  //   <ul class="list-group list-group-flush">
+  //     <li class="list-group-item">Address: ${address}</li>
+  //     <li class="list-group-item">Accepting new patients: ${acceptingNewPatience}</li>
+  //   </ul>
+  //   </div>
+  // </div>`;
+
+  console.log(`this is a last name: ${doctorLastName}`);
+  $('#doctors').text = doctorLastName;
+};
+
 $(document).ready(function () {
   $('#doctorForm').submit(function (event) {
     event.preventDefault();
@@ -22,13 +40,11 @@ $(document).ready(function () {
         doctor.practices.forEach(function (practice) {
           if (practice.within_search_area) {
             const acceptingNewPatience = practice.accepts_new_patients;
-            console.log(`Accepting New Patience: ${acceptingNewPatience}`);
             const address = `${practice.visit_address.city} ${practice.visit_address.state}, ${practice.visit_address.zip}
             \n${practice.visit_address.street}`;
-            console.log(address);
+            generateDoctorCard(firstName, lastName, image, acceptingNewPatience, address);
           }
         });
-
       });
     }, function (error) {
 
