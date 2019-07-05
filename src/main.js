a19e181d64cd32a60537c475a5664fb561f5290e
Alex Siegberg <alex.siegberg@gmail.com>
2019-07-05 09:13:25 -0700

    add Readme w/spec and initial API code

--- src/main.js
+++ src/main.js
@@ -1,9 +1,22 @@
-import { sum } from './sample-class.js';
+import { DoctorList } from './doctors.js';
 import './styles.css';
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 $(document).ready(function () {
+  $('#doctorForm').submit(function (event) {
+    event.preventDefault();
 
+    let service = new DoctorList();
+    let promise = service.getDoctor();
+
+    promise.then(function (response) {
+      let body = JSON.parse(response);
+      $('.output').text(body);
+    }, function (error) {
+
+      $('.output').text('Error message');
+    });
+  });
 });
