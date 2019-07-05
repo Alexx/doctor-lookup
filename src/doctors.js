4d574dcf8c3f42876ae09b7d53994f54fdcb556c
Alex Siegberg <alex.siegberg@gmail.com>
2019-07-05 10:38:07 -0700

    continue to extract doctor info from api

--- src/doctors.js
+++ src/doctors.js
@@ -1,8 +1,8 @@
 class DoctorList{
-  getDoctor() {
+  getDoctor(userQuery) {
     return new Promise(function (resolve, reject) {
       let request = new XMLHttpRequest();
-      const url = `https://api.betterdoctor.com/2016-03-01/doctors?query=tooth%20ache&location=45.520%2C-122.677%2C100&user_location=45.520%2C-122.677&skip=0&limit=10&user_key=${process.env.apiKey}`;
+      const url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${userQuery}&location=45.520%2C-122.677%2C100&user_location=45.520%2C-122.677&skip=0&limit=10&user_key=${process.env.apiKey}`;
       request.onload = function () {
         if (this.status === 200) {
           resolve(request.response);
--- src/main.js
+++ src/main.js
@@ -7,9 +7,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 $(document).ready(function () {
   $('#doctorForm').submit(function (event) {
     event.preventDefault();
-
+    const query = $('#medIssue').val();
     let service = new DoctorList;
-    let promise = service.getDoctor();
+    let promise = service.getDoctor(query);
 
     promise.then(function (response) {
       let body = JSON.parse(response);
@@ -18,9 +18,11 @@ $(document).ready(function () {
         const firstName = doctor.profile.first_name;
         const lastName = doctor.profile.last_name;
         const image = doctor.profile.image_url;
-
+        console.log(`${firstName} ${lastName}\n${image}`);
         doctor.practices.forEach(function (practice) {
           if (practice.within_search_area) {
+            const acceptingNewPatience = practice.accepts_new_patients;
+            console.log(`Accepting New Patience: ${acceptingNewPatience}`);
             const address = `${practice.visit_address.city} ${practice.visit_address.state}, ${practice.visit_address.zip}
             \n${practice.visit_address.street}`;
             console.log(address);
