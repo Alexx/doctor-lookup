59d33acb095d44f27dce00851c35ac1c826ab72e
Alex Siegberg <alex.siegberg@gmail.com>
2019-07-05 16:33:34 -0700

    convert api key to proper name

--- src/doctorAPI.js
+++ src/doctorAPI.js
@@ -5,8 +5,8 @@ class DoctorPull{
       let url;
       const frontAPI = 'https://api.betterdoctor.com/2016-03-01/doctors';
       const backAPI = '&location=45.520%2C-122.677%2C100&user_location=45.520%2C-122.677&skip=0&limit=10&user_key=';
-      
-      nameQuery === '' ? url = `${frontAPI}?query=${conditionQuery}${backAPI}${process.env.apiKey}` : url = `${frontAPI}?name=${nameQuery}&query=${conditionQuery}${backAPI}${process.env.apiKey}`;
+
+      nameQuery === '' ? url = `${frontAPI}?query=${conditionQuery}${backAPI}${process.env.exports.apiKey}` : url = `${frontAPI}?name=${nameQuery}&query=${conditionQuery}${backAPI}${process.env.exports.apiKey}`;
 
       request.onload = function () {
 
