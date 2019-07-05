b547fa0bbc48bf8e234d8cd1d4fecc417a84a26f
Alex Siegberg <alex.siegberg@gmail.com>
2019-07-05 12:42:49 -0700

    refactor generateDoctorCards function to use an array

--- src/main.js
+++ src/main.js
@@ -4,26 +4,30 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-let generateDoctorCard = (doctorFirstName, doctorLastName, image, acceptingNewPatience, address) => {
-  let htmlForDoctor =
-  `<div class='col-md-3'>
-  <div class="card">
-  <img class="card-img-top" src="${image}" alt="Card image cap">
-  <div class="card-body">
-  <h5 class="card-title">${doctorFirstName} ${doctorLastName}</h5>
-  </div>
-  <ul class="list-group list-group-flush">
-  <li class="list-group-item">Address: ${address}</li>
-  <li class="list-group-item">Accepting new patients: ${acceptingNewPatience}</li>
-  </ul>
-  </div>
-  </div>`;
+let generateDoctorCards = (docArray) => {
+  console.log(docArray);
+  docArray.forEach(function(doctor) {
+    let htmlForDoctor =
+    `<div class='col-md-3'>
+    <div class="card">
+    <img class="card-img-top" src="${doctor[2]}" alt="Card image cap">
+    <div class="card-body">
+    <h5 class="card-title">${doctor[0]} ${doctor[1]}</h5>
+    </div>
+    <ul class="list-group list-group-flush">
+    <li class="list-group-item">Address: ${doctor[3]}</li>
+    <li class="list-group-item">Accepting new patients: ${doctor[4]}</li>
+    </ul>
+    </div>
+    </div>`;
 
-  $('#doctors').append(htmlForDoctor);
+    $('#doctors').append(htmlForDoctor);
+  })
 };
 
 $(document).ready(function () {
   $('#doctorForm').submit(function (event) {
+    let doctorArray = [];
     event.preventDefault();
     $('#doctors').html('');
     const query = $('#medIssue').val();
@@ -40,18 +44,20 @@ $(document).ready(function () {
         let address;
         let acceptingNewPatience;
         doctor.practices.forEach(function (practice) {
-          debugger;
           if (practice.within_search_area) {
             acceptingNewPatience = practice.accepts_new_patients;
             address = `${practice.visit_address.city} ${practice.visit_address.state}, ${practice.visit_address.zip}\n${practice.visit_address.street}`;
           }
         });
+        doctorArray.push([firstName, lastName, image, address, acceptingNewPatience]);
 
-        generateDoctorCard(firstName, lastName, image, acceptingNewPatience, address);
       });
+      generateDoctorCards(doctorArray);
     }, function (error) {
 
       $('.output').text('Error message');
     });
+
+
   });
 });
