4aa51eea243a0e6a9c1cd65016a5a887f5bfb1bf
Alex Siegberg <alex.siegberg@gmail.com>
2019-07-05 14:51:20 -0700

    add ternary to add name search functionaility

--- src/doctorAPI.js
+++ src/doctorAPI.js
@@ -2,7 +2,9 @@ class DoctorPull{
   getDoctor(nameQuery, conditionQuery) {
     return new Promise(function (resolve, reject) {
       let request = new XMLHttpRequest();
-      const url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${nameQuery}&location=45.520%2C-122.677%2C100&user_location=45.520%2C-122.677&skip=0&limit=10&user_key=${process.env.apiKey}`;
+      let url;
+      nameQuery === '' ? url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${conditionQuery}&location=45.520%2C-122.677%2C100&user_location=45.520%2C-122.677&skip=0&limit=10&user_key=${process.env.apiKey}` : url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${nameQuery}&query=${conditionQuery}&location=45.520%2C-122.677%2C100&user_location=45.520%2C-122.677&skip=0&limit=10&user_key=${process.env.apiKey}`;
+
       request.onload = function () {
         if (this.status === 200) {
           resolve(request.response);
