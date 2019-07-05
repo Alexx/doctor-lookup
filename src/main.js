0622ea7434ba40f50cd00bbd78c9e82ee956d322
Alex Siegberg <alex.siegberg@gmail.com>
2019-07-05 10:19:30 -0700

    add code to capture doctor's info from api

--- src/main.js
+++ src/main.js
@@ -14,6 +14,20 @@ $(document).ready(function () {
     promise.then(function (response) {
       let body = JSON.parse(response);
       console.log(body);
+      body.data.forEach(function (doctor) {
+        const firstName = doctor.profile.first_name;
+        const lastName = doctor.profile.last_name;
+        const image = doctor.profile.image_url;
+
+        doctor.practices.forEach(function (practice) {
+          if (practice.within_search_area) {
+            const address = `${practice.visit_address.city} ${practice.visit_address.state}, ${practice.visit_address.zip}
+            \n${practice.visit_address.street}`;
+            console.log(address);
+          }
+        });
+
+      });
     }, function (error) {
 
       $('.output').text('Error message');
